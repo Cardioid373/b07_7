76779e5322e963c49d76b6016682e9a55d933842
Cari Cai <cardioid373@gmail.com>
2023-11-22 13:32:11 -0500

    update newComplaint

--- app/src/main/java/com/example/demoapplication/StudentNewComplaintActivity.java
+++ app/src/main/java/com/example/demoapplication/StudentNewComplaintActivity.java
@@ -2,6 +2,7 @@ package com.example.demoapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +26,7 @@ public class StudentNewComplaintActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_complaint);
+        // Initialize Firebase
         databaseReference = FirebaseDatabase.getInstance().getReference("complaints");
 
 
@@ -36,22 +38,7 @@ public class StudentNewComplaintActivity extends AppCompatActivity {
         btnNewComplaintSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Submit the complaint to firebase
-                String complaint = getNewComplaint.getText().toString();
-                String subject = getNewComplaintSubject.getText().toString();
-                Complaints newComplaint = new Complaints(complaint);
-                databaseReference.child(subject).setValue(newComplaint, new DatabaseReference.CompletionListener() {
-                    @Override
-                    public void onComplete(DatabaseError error, DatabaseReference ref) {
-                        if(error == null) {
-                            Toast.makeText(StudentNewComplaintActivity.this, "Submission Successful!", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(StudentNewComplaintActivity.this, StudentComplaintsActivity.class));
-                            finish();
-                        } else {
-                            Toast.makeText(StudentNewComplaintActivity.this, "Submission Failed!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                submitComplaint(getNewComplaintSubject, getNewComplaint);
             }
         });
 
@@ -64,4 +51,33 @@ public class StudentNewComplaintActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void submitComplaint(EditText subjectEditText, EditText complaintEditText) {
+        String subject = subjectEditText.getText().toString().trim();
+        String complaintText = complaintEditText.getText().toString().trim();
+        if(TextUtils.isEmpty(subject)) {
+            Toast.makeText(StudentNewComplaintActivity.this, "Subject cannot be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(complaintText)) {
+            Toast.makeText(StudentNewComplaintActivity.this, "Complaint cannot be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Submit the complaint to Firebase
+        Complaints newComplaint = new Complaints(complaintText);
+        databaseReference.child(subject).setValue(newComplaint, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError error, DatabaseReference ref) {
+                if(error == null) {
+                    Toast.makeText(StudentNewComplaintActivity.this, "Submission Successful!", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(StudentNewComplaintActivity.this, StudentComplaintsActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(StudentNewComplaintActivity.this, "Submission Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 }
+
