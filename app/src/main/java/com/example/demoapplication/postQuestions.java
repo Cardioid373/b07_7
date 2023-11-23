68cffd145c57cbc86f7dcc4c42e669a4daf851c7
darrengee-exe <darrengee.exe@gmail.com>
2023-11-23 00:21:52 -0500

    Created postQuestions class and added Post Activity to Manifest file

---
+++ app/src/main/java/com/example/demoapplication/postQuestions.java
@@ -0,0 +1,22 @@
+package com.example.demoapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class postQuestions extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.post_questions, container, false);
+
+        return view;
+    }
+
+}
