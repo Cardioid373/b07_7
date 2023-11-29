eadebb2caf7b08065ea68d0baac9051a605d2594
Siyun Gu <gsy041029@gmail.com>
2023-11-29 09:13:59 -0500

    delete unused import statements

--- app/src/main/java/com/example/demoapplication/NotificationActivity.java
+++ app/src/main/java/com/example/demoapplication/NotificationActivity.java
@@ -8,10 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -19,7 +16,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.DatabaseError;
 
 import java.util.ArrayList;
 
--- app/src/main/java/com/example/demoapplication/NotificationCreator.java
+++ app/src/main/java/com/example/demoapplication/NotificationCreator.java
@@ -3,9 +3,7 @@ package com.example.demoapplication;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
