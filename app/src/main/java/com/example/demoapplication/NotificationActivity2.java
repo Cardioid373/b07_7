5b80a2a61202e14efb6b711e0994aa2a580cc049
Siyun Gu <gsy041029@gmail.com>
2023-11-28 23:35:54 -0500

    delete back for notification interface to keep read/unread effect

--- app/src/main/java/com/example/demoapplication/NotificationActivity2.java
+++ app/src/main/java/com/example/demoapplication/NotificationActivity2.java
@@ -14,8 +14,6 @@ public class NotificationActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification2);
 
-        Button btnNotificationBack2 = findViewById(R.id.NotificationBackButton2);
-
         Intent intent = getIntent();
         String notificationTitle = intent.getStringExtra("notificationTitle");
         String notificationContent = intent.getStringExtra("notificationContent");
@@ -25,11 +23,5 @@ public class NotificationActivity2 extends AppCompatActivity {
 
         titleView.setText(notificationTitle);
         contentView.setText(notificationContent);
-
-        btnNotificationBack2.setOnClickListener(view -> {
-            //Return to student pag
-            startActivity(new Intent(NotificationActivity2.this, NotificationActivity.class));
-            finish();
-        });
     }
 }
