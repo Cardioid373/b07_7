9574785edd4903b44f573bc488b3576ebc97cd9b
Siyun Gu <gsy041029@gmail.com>
2023-11-28 23:32:39 -0500

    add back for notification interface

--- app/src/main/java/com/example/demoapplication/NotificationActivity2.java
+++ app/src/main/java/com/example/demoapplication/NotificationActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NotificationActivity2 extends AppCompatActivity {
@@ -12,6 +13,8 @@ public class NotificationActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification2);
+
+        Button btnNotificationBack2 = findViewById(R.id.NotificationBackButton2);
 
         Intent intent = getIntent();
         String notificationTitle = intent.getStringExtra("notificationTitle");
@@ -22,5 +25,11 @@ public class NotificationActivity2 extends AppCompatActivity {
 
         titleView.setText(notificationTitle);
         contentView.setText(notificationContent);
+
+        btnNotificationBack2.setOnClickListener(view -> {
+            //Return to student pag
+            startActivity(new Intent(NotificationActivity2.this, NotificationActivity.class));
+            finish();
+        });
     }
 }
